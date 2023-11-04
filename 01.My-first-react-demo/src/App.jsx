19051c12e54c04b4734224c208d88757e3f85757
IvaSabotinova <96121572+IvaSabotinova@users.noreply.github.com>
2023-11-05 01:19:21 +0200

    First-React-Demo

---
+++ 01.My-first-react-demo/src/App.jsx
@@ -0,0 +1,25 @@
+import Header from "./components/Header"
+import OurServices from "./components/OurServices"
+import About from "./components/About"
+import Footer from "./components/Footer"
+import WhyUs from "./components/WhyUs"
+import OurTeam from "./components/OurTeam"
+import Customers from "./components/Customers"
+import Info from "./components/Info"
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <OurServices />
+      <About />
+      <WhyUs />
+      <OurTeam />
+      <Customers />
+      <Info />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
